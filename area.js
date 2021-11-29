34224945f472690e52b09aed8506442834124550
SinchanaMS <94738532+SinchanaMS@users.noreply.github.com>
2021-11-29 17:57:37 +0530

    added area.html and area.js

---
+++ area.js
@@ -0,0 +1,19 @@
+var sides = document.querySelectorAll(".sides")
+var checkAreaBtn = document.querySelector("#check-area")
+var areaOfTriangle = document.querySelector("#output")
+
+
+function calculateProdOfBaseAndHeight(a,b){
+    var prodOfBaseAndHeight = a*b
+
+    return prodOfBaseAndHeight
+}
+
+function calculateArea(){
+    var prodOfBaseAndHeight = calculateProdOfBaseAndHeight(Number(sides[0].value), Number(sides[1].value))
+    var triangleArea = prodOfBaseAndHeight/2
+
+    areaOfTriangle.innerText = "The area of the triangle is: " +triangleArea + "square centimeter"
+}
+
+checkAreaBtn.addEventListener("click", calculateArea)
